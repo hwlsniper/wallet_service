d743f0a37daa7f1fa02e9123b7a4c053a5d0a1a0
abill <48088936@qq.com>
2018-08-21 10:02:43 +0800

    Fixed #1 各种代币资产的精度需要获取保存到资产信息列表里 自动添加币发行量

--- etoken-comp-api/src/main/java/it/etoken/component/api/controller/CoinsController.java
+++ etoken-comp-api/src/main/java/it/etoken/component/api/controller/CoinsController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.http.annotation.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +29,6 @@ import it.etoken.base.common.result.MLResult;
 import it.etoken.base.common.result.MLResultList;
 import it.etoken.base.common.result.MLResultObject;
 import it.etoken.base.model.market.entity.Coins;
-import it.etoken.base.model.market.entity.PocketAsset;
 import it.etoken.base.model.market.vo.CoinTicker;
 import it.etoken.cache.service.CacheService;
 import it.etoken.component.api.eosrpc.ContractAccountInfo;
@@ -254,6 +252,7 @@ public class CoinsController extends BaseController{
 					jo.put("name", thisCoins.getName());
 					jo.put("contractAccount", thisCoins.getContractAccount());
 					jo.put("icon", thisCoins.getImg());
+					jo.put("precisionNumber", thisCoins.getPrecisionNumber());
 					jo.put("detail", thisCoins.getIntr());
 					jo.put("createDate", thisCoins.getCreatedate());
 					jo.put("updateDate", thisCoins.getModifydate());
@@ -316,6 +315,7 @@ public class CoinsController extends BaseController{
 			contract_account = contract_account.trim();
 			//检测合约账号和合约名是否正确
 			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("json", "true");
 			jsonObject.put("code", contract_account);
 			jsonObject.put("scope", name);
 			jsonObject.put("table", "stat");
@@ -354,6 +354,26 @@ public class CoinsController extends BaseController{
 			coins.setName(name);
 			coins.setCode(name.toLowerCase());
 			coins.setContractAccount(contract_account);
+			
+			JSONObject coinInfo = rows.getJSONObject(0);
+			
+			String supplyString = coinInfo.getString("supply");
+			String[] supplyArray = supplyString.split(" ");
+			String supply = supplyArray[0].trim();
+			
+			String max_supply_string = coinInfo.getString("max_supply");
+			String[] max_supply_array = max_supply_string.split(" ");
+			String max_supply = max_supply_array[0].trim();
+			
+			int precision_number = 0;
+			if(max_supply.indexOf(".") > 0) {
+				precision_number = max_supply.length() - max_supply.indexOf(".") - 1;
+			}
+			
+			coins.setMarke((new BigDecimal(supply)).longValue());
+			coins.setTotal((new BigDecimal(max_supply)).longValue());
+			coins.setPrecisionNumber(precision_number);
+			
 			MLResult result = coinsFacadeAPI4Write.saveUpdate(coins);
 
 			if (result.isSuccess()) {
@@ -398,6 +418,87 @@ public class CoinsController extends BaseController{
 		}
 	}
 
-	
+	/**
+	 * 修正数据
+	 * 
+	 * @param requestMap
+	 * @param request
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/fixedData")
+	public Object fixedData(@RequestParam Map<String, String> requestMap, HttpServletRequest request) {
+		try {
+			MLResultList<Coins> coinsResultList = coinsFacadeAPI.findAllCoins();
+			if(!coinsResultList.isSuccess()) {
+				return this.error(coinsResultList.getErrorCode(), coinsResultList.getErrorHint(), coinsResultList.getList());
+			}
+			List<Coins> coinsList = coinsResultList.getList();
+			for(Coins coins : coinsList) {
+				if(coins.getContractAccount() == null || coins.getContractAccount().isEmpty()) {
+					continue;
+				}
+				
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("json", "true");
+				jsonObject.put("code", coins.getContractAccount());
+				jsonObject.put("scope", coins.getName());
+				jsonObject.put("table", "stat");
+				
+				EosResult checkAccountResp = null;
+				try {
+					checkAccountResp = new ContractAccountInfo().run(URL_CHAIN, URL_CHAIN_BACKUP, jsonObject.toString());
+				}catch(Exception ex) {
+					continue;
+				}
+				if (!checkAccountResp.isSuccess()) {
+					return this.error(MLApiException.CONTRACT_NOT_EXIST_ERROR, checkAccountResp.getData());
+				}
+				JSONObject checkJson = JSONObject.parseObject(checkAccountResp.getData());
+				JSONArray rows = checkJson.getJSONArray("rows");
+				if(null == rows || rows.isEmpty()) {
+					JSONObject checkError = checkJson.getJSONObject("error");
+					if(null == checkError) {
+						return this.error(MLApiException.CONTRACT_NOT_EXIST_ERROR, checkJson);
+					}
+					if(checkError.getIntValue("code")==3060002) {
+						 return this.error(MLApiException.CONTRACT_ACCOUNT_ERROR, checkJson);
+					 }else if(checkError.getIntValue("code")==10) {
+						 return this.error(MLApiException.CONTRACT_NAME_ERROR, checkJson);
+					 }
+					return this.error(MLApiException.CONTRACT_NOT_EXIST_ERROR, checkJson);
+				}
+				
+				JSONObject coinInfo = rows.getJSONObject(0);
+				
+				String supplyString = coinInfo.getString("supply");
+				String[] supplyArray = supplyString.split(" ");
+				String supply = supplyArray[0].trim();
+				
+				String max_supply_string = coinInfo.getString("max_supply");
+				String[] max_supply_array = max_supply_string.split(" ");
+				String max_supply = max_supply_array[0].trim();
+				
+				int precision_number = 0;
+				if(max_supply.indexOf(".") > 0) {
+					precision_number = max_supply.length() - max_supply.indexOf(".") - 1;
+				}
+				
+				coins.setMarke((new BigDecimal(supply)).longValue());
+				coins.setTotal((new BigDecimal(max_supply)).longValue());
+				coins.setPrecisionNumber(precision_number);
+				
+				MLResult result = coinsFacadeAPI4Write.saveUpdate(coins);
+
+				if (!result.isSuccess()) {
+					return this.error(result.getErrorCode(), result.getErrorHint(), "");
+				}
+			}
+			return this.success(true);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return this.error(MLApiException.CONTRACT_EXIST_ERROR, null);
+		}
+	}
 
 }
